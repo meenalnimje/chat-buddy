1978dd42be30e792f5be0a764d63ce764aff7595
meenal <meenalnimje@gmail.com>
2024-09-18 20:36:11 +0530

    mongodb url added to env

--- mongodb/index.js
+++ mongodb/index.js
@@ -11,9 +11,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(
-      "mongodb+srv://21mm02007:21mm02007@cluster0.cpreblc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGODB_URL);
 
     isConnected = true;
 
